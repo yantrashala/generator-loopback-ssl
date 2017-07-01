317cb5766bac07315c011a7b15b083b4dbd9ff76
Siddhartha Lahiri <slahiri@sapient.com>
2017-07-01 22:09:48 +0530

    ~ added yarn support ~ added base utilities

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,36 +1,24 @@
 'use strict';
+
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const parseAuthor = require('parse-author');
+const githubUsername = require('github-username');
+const path = require('path');
+const askName = require('inquirer-npm-name');
+const pkg = require('../../package.json');
+const _ = require('lodash');
+const promise = require("bluebird");
 
 module.exports = class extends Generator {
   prompting() {
-    // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the bedazzling ' + chalk.red('generator-loopback-ssl') + ' generator!'
+      'Welcome to '
+        + chalk.yellow(this.rootGeneratorName())
+        +' Version: '
+        + chalk.red(this.rootGeneratorVersion())
     ));
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
-
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
-
-  install() {
-    this.installDependencies();
-  }
+  };
 };
