1fef431b2d67f4e13f97a645a61192056538e009
Siddhartha Lahiri <slahiri@sapient.com>
2017-07-08 21:30:37 +0530

    ~ completed prompt sequence ~ validate target package ~ Known Issue: Unit Tests Failing ~ removed gulp file - unnecessary ~ added utility function

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
@@ -6,7 +7,17 @@ var helpers = require('yeoman-test');
 describe('generator-loopback-ssl:app', () => {
   beforeAll(() => {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
+      .withPrompts({name: 'gen-test',
+        description: '',
+        version: '1.0.0',
+        homepage: undefined,
+        loopbackVersion: '2.x',
+        httpMode: 'option2',
+        certificatePath: '/certificate/path/',
+        privateKey: 'key.pem',
+        certificate: 'cert.pem',
+        generateCertificate: false,
+        confirmSetup: true});
   });
 
   it('creates files', () => {
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,68 +1,147 @@
 'use strict';
 
 const Generator = require('yeoman-generator');
-const chalk = require('chalk');
+const debug = require('debug')('loopback-ssl:index');
 const yosay = require('yosay');
-const parseAuthor = require('parse-author');
-const githubUsername = require('github-username');
+// Const chalk = require('chalk');
+const _util = require('../../lib/utility.js');
+// Const parseAuthor = require('parse-author');
+// const githubUsername = require('github-username');
 const path = require('path');
-const askName = require('inquirer-npm-name');
-const pkg = require('../../package.json');
+// Const askName = require('inquirer-npm-name');
+// const pkg = require('../../package.json');
 const _ = require('lodash');
-const promise = require("bluebird");
-const pkgJson = require('../../package.json');
+const extend = _.merge;
+// Const pkgJson = require('../../package.json');
 
 module.exports = class extends Generator {
-  prompting() {
-    return this.prompt([{
-      type    : 'list',
-      name    : 'loopbackVersion',
-      message : 'Select loopback version',
-      choices : ['2.x', '3.x'],
-      default : '3.x'
+  initializing() {
+    this.pkg = this.fs.readJSON(this.destinationPath('package.json'), {});
+    this.props = {
+      name: this.pkg.name,
+      description: this.pkg.description,
+      version: this.pkg.version,
+      homepage: this.pkg.homepage
+    };
+  }
+
+  _askForModuleName() {
+    if (this.pkg.name || this.options.name) {
+      this.props.name = this.pkg.name || _.kebabCase(this.options.name);
+      debug('Package Name;', this.pkg.name);
+      return Promise.resolve();
+    }
+
+    return ({
+      name: 'name',
+      message: 'Module Name',
+      default: path.basename(process.cwd()),
+      filter: _.kebabCase,
+      validate(str) {
+        return str.length > 0;
+      }
+    }, this).then(answer => {
+      this.props.name = answer.name;
+    });
+  }
+
+  _askFor() {
+    const prompts = [{
+      type: 'list',
+      name: 'loopbackVersion',
+      message: 'Select loopback version',
+      choices: ['2.x', '3.x'],
+      default: '2.x'
     }, {
-      type    : 'list',
-      name    : 'httpMode',
-      message : 'Select HTTP mode',
-      choices : [{
+      type: 'list',
+      name: 'httpMode',
+      message: 'Select HTTP mode',
+      choices: [{
         value: 'option1',
-        name: 'Option 1: HTTP: Default loopback configuration'
-      },{
+        name: 'Option 1: HTTP : Default loopback configuration'
+      }, {
         value: 'option2',
-        name: 'Option 2: HTTPS: Loading certificates at runtime'
-      },{
+        name: 'Option 2: HTTPS: Loading certificates from file'
+      }, {
         value: 'option3',
         name: 'Option 3: HTTPS: Generating Certificates at runtime'
+      }, {
+        value: 'option4',
+        name: 'Option 4: HTTPS: Mutual SSL authentication'
       }]
     }, {
-      when    : function(response) {
-        return ((response.httpMode !== 'option1') && (response.httpMode !== 'option3'));
+      when: function (response) {
+        return ((response.httpMode !== 'option1') &&
+          (response.httpMode !== 'option3'));
       },
-      type    : 'input',
-      name    : 'certificatePath',
-      message : 'Provide certificate path',
-      default : '/certificate/path/'
+      type: 'input',
+      name: 'certificatePath',
+      message: 'Provide certificate path',
+      default: '/certificate/path/'
     }, {
-      when    : function(response) {
-        return ((response.httpMode !== 'option1') && (response.httpMode !== 'option3'));
+      when: function (response) {
+        return ((response.httpMode !== 'option1') &&
+          (response.httpMode !== 'option3'));
       },
-      type    : 'input',
-      name    : 'privateKey',
-      message : 'Provide key name',
-      default : 'key.pem'
+      type: 'input',
+      name: 'privateKey',
+      message: 'Private Key',
+      default: 'key.pem'
     }, {
-      when    : function(response) {
-        return ((response.httpMode !== 'option1') && (response.httpMode !== 'option3'));
+      when: function (response) {
+        return ((response.httpMode !== 'option1') &&
+          (response.httpMode !== 'option3'));
       },
-      type    : 'input',
-      name    : 'certificate',
-      message : 'Certificate name',
-      default : 'cert.pem'
-    }]).then((answers) => {
-      this.log('loopback version', answers.loopbackVersion);
-      this.log('httpMode', answers.httpMode);
-      this.log('certificate path', answers.certificatePath);
-      this.log('certificate path', answers.privateKey);
+      type: 'input',
+      name: 'certificate',
+      message: 'Certificate',
+      default: 'cert.pem'
+    }, {
+      when: function (response) {
+        return ((response.httpMode !== 'option1') &&
+          (response.httpMode !== 'option3') &&
+          (response.httpMode === 'option2'));
+      },
+      type: 'confirm',
+      name: 'generateCertificate',
+      message: 'Do you want to generate self signed certificates?',
+      default: false
+    }, {
+      when: function (response) {
+        return ((response.httpMode !== 'option1') &&
+          (response.httpMode !== 'option3') &&
+          (response.httpMode === 'option4'));
+      },
+      type: 'input',
+      name: 'clientCertificate',
+      message: 'Client Certificate',
+      default: 'client-cert.pem'
+    }, {
+      type: 'confirm',
+      name: 'confirmSetup',
+      message: 'Do you want to continue with the above settings?',
+      default: true
+    }];
+
+    return this.prompt(prompts).then(props => {
+      this.props = extend(this.props, props);
+      debug(this.props);
     });
+  }
+
+  prompting() {
+    _util.validateTarget(this.pkg);
+
+    console.log(yosay('Hello, and welcome to loopback-ssl generator!'));
+
+    return this._askForModuleName()
+      .then(this._askFor.bind(this));
+  }
+
+  writing() {
+    this.fs.copy(
+      this.templatePath('dummyfile.txt'),
+      this.destinationPath('dummyfile.txt')
+    );
   }
 };
