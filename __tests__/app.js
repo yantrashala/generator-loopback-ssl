39f0984c6946c3ef3837c0259d8ae2b833155b27
Siddhartha Lahiri <slahiri@sapient.com>
2017-07-13 01:54:42 +0530

    ~ added loopback 2 and loopback 3 fixtures for unit testing

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,11 +3,14 @@
 var path = require('path');
 var yassert = require('yeoman-assert');
 var helpers = require('yeoman-test');
+var fs = require('fs-extra');
 
 describe('generator-loopback-ssl:app', () => {
   beforeAll(() => {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({name: 'gen-test',
+      .inTmpDir(function (dir) {
+        fs.copySync(path.join(__dirname, './fixtures/loopback-2'), dir)
+      }).withPrompts({name: 'gen-test',
         description: '',
         version: '1.0.0',
         homepage: undefined,
@@ -17,7 +20,8 @@ describe('generator-loopback-ssl:app', () => {
         privateKey: 'key.pem',
         certificate: 'cert.pem',
         generateCertificate: false,
-        confirmSetup: true});
+        confirmSetup: true
+      });
   });
 
   it('creates files', () => {
