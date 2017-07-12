786ac7c67d6fffce7c3aa441025b11c51b3ba3b2
Siddhartha Lahiri <slahiri@sapient.com>
2017-07-12 14:51:01 +0530

    ~ upgraded eslint, yeoman-test, node-emoji and timestamp ~ modularized code and added utility methods

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,15 +1,14 @@
 'use strict';
 
 const Generator = require('yeoman-generator');
-const debug = require('debug')('loopback-ssl:index');
+const debug = require('debug')('generator-loopback-ssl:index');
 const yosay = require('yosay');
 const chalk = require('chalk');
 const _util = require('../../lib/utility.js');
 const path = require('path');
 const _ = require('lodash');
 const extend = _.merge;
-const emoji = require('node-emoji');
-const lbPkg = require('../../package.json');
+const timestamp = require('timestamp');
 
 module.exports = class extends Generator {
   initializing() {
@@ -50,6 +49,11 @@ module.exports = class extends Generator {
       choices: ['2.x', '3.x'],
       default: '2.x'
     }, {
+      type: 'input',
+      name: 'configFile',
+      message: 'Loopback configuration file to update (server folder):',
+      default: 'config.json'
+    }, {
       type: 'list',
       name: 'httpMode',
       message: 'Select loopback-ssl configuration option',
@@ -58,72 +62,49 @@ module.exports = class extends Generator {
         name: 'Option 1: HTTP : Default loopback configuration'
       }, {
         value: 'option2',
-        name: 'Option 2: HTTPS: Loading certificates from file'
+        name: 'Option 2: HTTPS: Loading certificates from files'
       }, {
         value: 'option3',
-        name: 'Option 3: HTTPS: Generating Certificates at runtime'
-      }, {
-        value: 'option4',
-        name: 'Option 4: HTTPS: Mutual SSL authentication'
+        name: 'Option 3: HTTPS: Loading certificates from files & Mutual SSL authentication'
       }]
     }, {
       when: function (response) {
-        return ((response.httpMode !== 'option1') &&
-          (response.httpMode !== 'option3'));
+        return (response.httpMode !== 'option1');
       },
       type: 'input',
       name: 'certificatePath',
       message: 'Provide certificate path',
-      default: '/certificate/path/',
-      store: true
+      default: '/server/certs/'
     }, {
       when: function (response) {
-        return ((response.httpMode !== 'option1') &&
-          (response.httpMode !== 'option3'));
+        return (response.httpMode !== 'option1');
       },
       type: 'input',
       name: 'privateKey',
       message: 'Private Key',
-      default: 'key.pem',
-      store: true
+      default: 'key.pem'
     }, {
       when: function (response) {
-        return ((response.httpMode !== 'option1') &&
-          (response.httpMode !== 'option3'));
+        return (response.httpMode !== 'option1');
       },
       type: 'input',
       name: 'certificate',
       message: 'Certificate',
-      default: 'cert.pem',
-      store: true
+      default: 'cert.pem'
     }, {
       when: function (response) {
-        return ((response.httpMode !== 'option1') &&
-          (response.httpMode !== 'option3') &&
-          (response.httpMode === 'option2'));
-      },
-      type: 'confirm',
-      name: 'generateCertificate',
-      message: 'Do you want to generate self signed certificates?',
-      default: false
-    }, {
-      when: function (response) {
-        return ((response.httpMode !== 'option1') &&
-          (response.httpMode !== 'option3') &&
-          (response.httpMode === 'option4'));
+        return (response.httpMode === 'option3');
       },
       type: 'input',
       name: 'clientCertificate',
       message: 'Client Certificate',
-      default: 'client-cert.pem',
-      store: true
+      default: 'client-cert.pem'
     }, {
       type: 'confirm',
       name: 'confirmSetup',
-      message: emoji.emojify(':warning: ') +
-        chalk.yellow(' Installing loopback-ssl will replace the existing server.js') +
-        '\n  Do you want to continue with the above settings?',
-      default: true
+      message: function (message) {
+        return _util.confirmSetupCaption(message);
+      }
     }];
 
     return this.prompt(prompts).then(props => {
@@ -133,23 +114,38 @@ module.exports = class extends Generator {
   }
 
   prompting() {
-    _util.validateTarget(this.pkg);
+    _util.validateTargetPackage(this.pkg);
     console.log(yosay('Hello, and welcome to loopback-ssl generator!'));
     return this._askForModuleName()
       .then(this._askFor.bind(this));
   }
 
   writing() {
-    const currentPkg = this.fs.readJSON(this.destinationPath('package.json'), {});
-    const lbSSLPkg = {
-      dependencies: {
-        'loopback-ssl': lbPkg.peerDependencies['loopback-ssl']
-      }
-    };
-    const pkg = extend(lbSSLPkg, currentPkg);
-    debug('Updated Package: ', pkg);
     if (this.props.confirmSetup === true) {
+      _util.validateTargetPackage(this.pkg);
+      const pkg = _util.addLoopbackSSLDependency(this.pkg);
+      debug('Target Package: ', pkg);
+
+      _util.validateTargetConfigFile(this.destinationPath('server/' + this.props.configFile));
+      const currentConfig = this.fs.readJSON(this.destinationPath('server/' + this.props.configFile), {});
+      const targetConfig = _util.addLoopbackSSLConfig(currentConfig, this.props);
+      debug('Target Config: ', targetConfig);
+
+      this.fs.copy(
+        this.destinationPath('server/server.js'),
+        this.destinationPath('server/_backup_' + timestamp() + '_server.js')
+      );
+      this.fs.copy(
+        this.destinationPath('server/' + this.props.configFile),
+        this.destinationPath('server/_backup_' + timestamp() + '_' + this.props.configFile)
+      );
+
+      this.fs.copy(
+        this.templatePath('server/server.js'),
+        this.destinationPath('server/server.js')
+      );
       this.fs.writeJSON(this.destinationPath('package.json'), pkg);
+      this.fs.writeJSON(this.destinationPath('server/' + this.props.configFile), targetConfig);
     }
   }
 
